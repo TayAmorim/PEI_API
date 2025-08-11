00fa9e90af2f4aa2c85392105d54abb02a86992e
Tayanna Amorim <105131804+TayAmorim@users.noreply.github.com>
2025-08-11 23:04:35 +0000

    :sparkles: feat: adicionando configuracao para login

--- src/routes/activies.ts
+++ src/routes/activies.ts
@@ -4,11 +4,10 @@ async function routes (fastify, options) {
     fastify.get('/', async (request, reply) => {
       try {
         const supabase = request.server.supabase as SupabaseClient
-        console.log('supabase', supabase)
         const {data, error} = await supabase.from('atividades').select("*")
 
         if (error) { throw new Error("Algum erro detectado", error)}
-        return { message: 'Conexão com Supabase a ser testada aqui!', data }
+        return { message: 'Listagem retornada com sucesso', data }
       } catch (e) {
           reply.code(500).send({ error: 'Erro ao conectar com o Supabase' })
       }
--- src/routes/server.ts
+++ src/routes/server.ts
@@ -1,16 +1,25 @@
-
+import dotenv from 'dotenv'
+dotenv.config()
 import Fastify from 'fastify'
+import cors from '@fastify/cors'
 import supabasePlugin from '../plugins/supabase.js'
 import routes from './activies.js'
+import authenticate from '../plugins/authenticate.js'
 
-
+const port = Number(process.env.PORT)|| 3000;
 const fastify = Fastify({
   logger: true
 })
+
+await fastify.register(cors, {
+  origin: true,
+})
+
+fastify.addHook('onRequest', authenticate)
 fastify.register(supabasePlugin)
 fastify.register(routes)
 
-fastify.listen({ port: 3000 }, function (err, address) {
+fastify.listen({ port, host: '0.0.0.0' }, function (err, address) {
   if (err) {
     fastify.log.error(err)
     process.exit(1)
