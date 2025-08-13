9d8023856f4d24800239a687dca5e61b81d50616
Tayanna Amorim <105131804+TayAmorim@users.noreply.github.com>
2025-08-13 23:10:17 +0000

    fix: alinhamento final da configuração de deploy

--- src/routes/server.ts
+++ src/routes/server.ts
@@ -1,45 +1,39 @@
-import Fastify from 'fastify'
-import cors from '@fastify/cors'
-import {fastifyEnv} from '@fastify/env'
-import supabasePlugin from '../plugins/supabase.js'
-import activityRoutes from './activies.js'
-// import authenticate from '../plugins/authenticate.js' // Se precisar, descomente
-import rootRoutes from './rootRutes.js'
-import { schema } from '../types/env.types.js'
+import Fastify from 'fastify';
+import dotenv from 'dotenv';
+import cors from '@fastify/cors';
+// import authenticate from '../plugins/authenticate.js' 
+import supabasePlugin from '../plugins/supabase.js';
+import rootRoutes from './rootRoutes.js'; 
+import activityRoutes from './activies.js'; 
 
 
+dotenv.config({ path: '.env' });
 
 const app = Fastify({
-  logger: true
-})
+  logger: true,
+});
 
-app.register(cors, { origin: true })
-app.register(supabasePlugin)
-// app.addHook('onRequest', authenticate) // Descomente para proteger todas as rotas
-app.register(rootRoutes)
-app.register(activityRoutes, { prefix: 'activities' })
 
-await app.register(fastifyEnv, {
-    confKey: 'init', 
-    schema: schema,
-    dotenv: true 
-  });
+app.register(cors, { origin: true });
+app.register(supabasePlugin);
+// app.addHook('onRequest', authenticate)
+app.register(rootRoutes);
+app.register(activityRoutes, { prefix: 'activities' });
 
-export default async (req, res) => {
-  await app.ready()
-  app.server.emit('request', req, res)
-}
-
-if (process.env.STAGE === 'dev') {
+if (process.env.NODE_ENV === 'development') {
   const start = async () => {
     try {
-      const port = 3000
-      await app.listen({ port: port, host: '0.0.0.0' })
-      console.log(`🚀 Servidor local rodando em http://localhost:${port}`)
+      const port = Number(process.env.PORT) || 3000;
+      await app.listen({ port, host: '0.0.0.0' });
     } catch (err) {
-      app.log.error(err)
-      process.exit(1)
+      app.log.error(err);
+      process.exit(1);
     }
-  }
-  start()
+  };
+  start();
 }
+
+export default async (req, res) => {
+  await app.ready();
+  app.server.emit('request', req, res);
+};
